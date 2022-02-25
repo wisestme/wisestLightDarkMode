78e9ea6a56ed8ee791ff7ae94d029cf1fdf35f2c
wisestme <wyhzest@gmail.com>
2022-02-25 15:24:25 +0100

    add mode toggle elements

---
+++ main.js
@@ -0,0 +1,13 @@
+// let switchToggle = document.querySelector('input[type="checkbox"]');
+
+
+// function switchMode (event) {
+//     if(event.target.checked) {
+//         document.documentElement.setAttribute('data-theme', 'dark');
+
+//     } else {
+//         document.documentElement.setAttribute('data-theme', 'light');
+//     }
+// }
+
+// switchToggle.addEventListener('change', switchMode);
