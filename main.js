47ce82aea18e436647469ad74710fc716b402c42
wisestme <wyhzest@gmail.com>
2022-02-25 15:31:22 +0100

    change mode icon

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 let switchToggle = document.querySelector('input[type="checkbox"]');
-
+let modeIcon = document.querySelector('.fas');
 
 function switchMode (event) {
     if(event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-
+        modeIcon.classList.replace('fa-sun', 'fa-moon');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        modeIcon.classList.replace('fa-moon', 'fa-sun');
     }
 }
 
