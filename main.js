cc111fb9a7d2ba9df5c257dd0cb0882383b1d325
wisestme <wyhzest@gmail.com>
2022-02-28 14:01:17 +0100

    refactor code

--- main.js
+++ main.js
@@ -6,24 +6,28 @@ let images = ['undraw_experience_design_eq-3-j',
             'undraw_usability_testing_re_uu1g',
             'undraw_web_devices_re_m8sc'];
 
+
+function imageSelect (mode) {
 let allImages = document.querySelectorAll('[class*="svg_image"]');
-console.log(allImages);
 
-allImages.forEach((element,index) => {
-    
-let imageSrc = `${imagePath}${images[index]}${svg}`;
-
-    element.setAttribute('src', imageSrc);
-});
-
-function switchMode (event) {
-    if(event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        modeIcon.classList.replace('fa-sun', 'fa-moon');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        modeIcon.classList.replace('fa-moon', 'fa-sun');
-    }
+    allImages.forEach((element,index) => {
+        let imageSrc = `${imagePath}${images[index]}${mode}${svg}`;
+            element.setAttribute('src', imageSrc);
+        });
 }
 
-switchToggle.addEventListener('change', switchMode);
+imageSelect('')
+
+function switchMode (event) {
+        if(event.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            modeIcon.classList.replace('fa-sun', 'fa-moon');
+            imageSelect('_dark')
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            modeIcon.classList.replace('fa-moon', 'fa-sun');        
+            imageSelect('')                    ;
+            }
+    }
+
+    switchToggle.addEventListener('change', switchMode);
